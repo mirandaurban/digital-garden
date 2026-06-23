da590c8f98c9fd688ce54dfc32f8558490fa89d5
Miranda Urban Solano <a01752391@tec.mx>
2026-06-23 10:29:41 -0600

    feature: Math plugin added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
 });
